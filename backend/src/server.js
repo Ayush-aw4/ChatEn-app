842cc0062debfa52d1eed18a403a7f78315bb3c6
AYUSH AWCHAR <ayushawchar9403@gmail.com>
2026-06-01 20:56:37 +0530

    login & log out done

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js"
 
-dotenv.config();
+
 const app = express();
 const __dirname = path.resolve();
 const PORT =ENV.PORT || 3000;
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 
-if(process.env.NODE_ENV === "production"){
+if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get("*",(_,res)=>{
         res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
