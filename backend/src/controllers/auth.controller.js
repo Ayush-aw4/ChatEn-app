942b8c8906248acfab00fc388c6d4d36d53b2aa6
AYUSH AWCHAR <ayushawchar9403@gmail.com>
2026-06-02 15:44:45 +0530

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -66,6 +66,9 @@ if(user) return res.status(400).json({message: "Email already exists"})
 
 export const login = async (req,res)=>{
     const {email,password} =req.body
+    if(!email || !password){
+        return res.status(400).json({message: "Email and password are required"})
+    }
     try{
         const user = await User.findOne({email})
         if(!user) return res.status(400).json({message:"Invalid Credentials"})
